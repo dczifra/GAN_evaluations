131ab864b1808c14f7bec0ef0dcb1c9b01520ac4
doma <doma945@gmail.com>
2019-02-13 12:45:29 +0100

    fix

---
+++ scripts/munkres_try.py
@@ -0,0 +1,5 @@
+import tensorflow as tf
+hungarian_module = tf.load_op_library('../munkres-tensorflow/hungarian.so')
+with tf.Session(''):
+  print("www")
+  print(hungarian_module.hungarian([[[1, 2], [3, 4],[3,4]]]).eval())
